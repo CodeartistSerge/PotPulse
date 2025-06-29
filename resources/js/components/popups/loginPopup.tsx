3c2a398b788d4f37c61f5dc6c3743695f6430699
Serge Degtiarev <ser6722@yandex.ru>
2025-06-29 15:50:55 +1200

    lint

--- resources/js/components/popups/loginPopup.tsx
+++ resources/js/components/popups/loginPopup.tsx
@@ -1,6 +1,6 @@
 import Popup from '@/components/popups/popup';
 
-import { Link, useForm, usePage } from '@inertiajs/react';
+import { Link, useForm } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 import { FormEventHandler } from 'react';
 import InputError from '@/components/input-error';
@@ -12,7 +12,6 @@ import Label from '@/components/ui/label';
 import { cn } from '@/lib/utils';
 import { useDispatch } from 'react-redux';
 import { closePopup, openPopup } from '@/store/popups';
-import { SharedData } from '@/types';
 
 type LoginForm = {
     email: string;
@@ -27,8 +26,6 @@ interface LoginProps {
 }
 
 export default function LoginPopup({ visible, status, canResetPassword }: LoginProps) {
-	const { csrf } = usePage<SharedData>().props;
-
 	const { data, setData, post, processing, errors, reset } = useForm<Required<LoginForm>>({
 		email: '',
 		password: '',
